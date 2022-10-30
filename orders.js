480ef663fc2ab5cc29c4f0f9a26658a04ee6b218
Ifald <ihorfedoruk@gmail.com>
2022-10-30 21:21:47 +0200

    Orders

---
+++ orders.js
@@ -0,0 +1,32 @@
+const Orders = [
+    {
+        productName: 'Solim Flower',
+        productNumber: '20631',
+        paymentStatus: 'Due',
+        shiping: 'Pending'
+    },
+    {
+        productName: 'The English Garden',
+        productNumber: '20647',
+        paymentStatus: 'Paid',
+        shiping: 'Delivered'
+    },
+    {
+        productName: 'Margies Flowers',
+        productNumber: '20656',
+        paymentStatus: 'Due',
+        shiping: 'Pending'
+    },
+    {
+        productName: 'LaCatrina Flower Vase',
+        productNumber: '20662',
+        paymentStatus: 'Paid',
+        shiping: 'Pending'
+    },
+    {
+        productName: 'Daisy Flower Shop',
+        productNumber: '20674',
+        paymentStatus: 'Paid',
+        shiping: 'Delivered'
+    }
+];
